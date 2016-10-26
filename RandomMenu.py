b9b7deabb30df1e45e132b7110c68eac00262276
Tej <tjoshi@chadwickschool.org>
2016-10-26 15:40:03 -0700

    Changed the name of variables to match what they do

--- RandomMenu.py
+++ RandomMenu.py
@@ -4,23 +4,22 @@ list1 = ['spicy', 'creamy', 'mild', 'organic', 'sweet', 'sour', 'hot', 'salty', 
 list2 = ['steamed', 'fried', 'sauteed', 'baked', 'grilled', 'sliced', 'marinated', 'scrambled', 'roasted', 'simmered'];
 list3 = ['chicken', 'beef', 'pork', 'lamb', 'steak', 'tuna', 'bison', 'noodles', 'venison', 'salmon'];
 
-#for x in range(10):
-    #x == 0
-    #print(list1[x])
-    #x +1
-a = 10
-b = 10
-c = 10
+print("Welcome to Tej's restaurant!")
+print("Here is our menu for tonight:")
+
+list1range = 10
+list2range = 10
+list3range = 10
 
 for x in range(10):
-    x = randrange(0,a)
-    y = randrange(0,b)
-    z = randrange(0,c)
-    print(list1[x], list2[y], list3[z])
-    del list1[x]
-    del list2[y]
-    del list3[z]
-    a = a-1
-    b = b-1
-    c = c-1
+    adjective = randrange(0,list1range)
+    verb = randrange(0,list2range)
+    noun = randrange(0,list3range)
+    print(list1[adjective], list2[verb], list3[noun])
+    del list1[adjective]
+    del list2[verb]
+    del list3[noun]
+    list1range = list1range -1
+    list2range = list2range -1
+    list3range = list3range -1
   
